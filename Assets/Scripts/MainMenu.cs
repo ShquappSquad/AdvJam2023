3a2b3dbb9edd52c7f453d9c591aca86d42d1761a
unknown <shquapp@gmail.com>
2023-06-01 23:20:18 -0700

    Added Dialogue system
    -Added first dialogue before the forest

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {    
     public Animator animator;
     public int secondsToMusicFade = 2;
     public AudioSource audioMusic;
+    public Image dialogue, arrow;
+    private bool dialogueDone = false;
 
     //Called when the Play button is clicked
     public void PlayGame()
     {
         StartFadeOut(1);
+    }
+
+    public void Update()
+    {
+        if(dialogueDone == true)
+        {
+            if(Input.anyKeyDown)
+            {
+                Debug.Log("WORKING");
+                Invoke("StartSceneChange", 0.01f);
+                //StartSceneChange();
+            }
+        }
     }
 
     //Called when the Quit button is clicked
@@ -26,45 +42,49 @@ public class MainMenu : MonoBehaviour
         //Trigger fade to black animation then switch scenes after a delay
         animator.SetTrigger("FadeOut");
         findAudio(); 
+        Invoke("StartDialogue", 1f);
     }
 
     public void StartDialogue()
     {
-        Invoke("StartSceneChange", 1f);
+        dialogueDone = true;
+        dialogue.transform.localScale = new Vector3(10.5f, 2.75f, 0f);
+        arrow.transform.localScale = new Vector3(10.5f, 2.75f, 0f);
     }
     
     //Change to next scene
-    public void StartSceneChange()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+        public void StartSceneChange()
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
 
     //Fade Music
-    public void findAudio()
-    {
-        // Call findAudioAndFadeOut Coroutine
-        StartCoroutine(findAudioAndFadeOut());
-    }
-
-    IEnumerator findAudioAndFadeOut()
-    {
-        // Find Audio Music in scene
-        audioMusic = GameObject.Find("Main Menu").GetComponent<AudioSource>();
- 
-        // Check Music Volume and Fade Out
-        while (audioMusic.volume > 0.01f)
+        public void findAudio()
         {
-            audioMusic.volume -= Time.deltaTime / secondsToMusicFade;
-            yield return null;
+            // Call findAudioAndFadeOut Coroutine
+            StartCoroutine(findAudioAndFadeOut());
         }
- 
-        // Make sure volume is set to 0
-        audioMusic.volume = 0;
- 
-        // Stop Music
-        audioMusic.Stop();
- 
-        // Destroy
-        Destroy (this);
-    }
+
+        IEnumerator findAudioAndFadeOut()
+        {
+            // Find Audio Music in scene
+            audioMusic = GameObject.Find("Main Menu").GetComponent<AudioSource>();
+    
+            // Check Music Volume and Fade Out
+            while (audioMusic.volume > 0.01f)
+            {
+                audioMusic.volume -= Time.deltaTime / secondsToMusicFade;
+                yield return null;
+            }
+    
+            // Make sure volume is set to 0
+            audioMusic.volume = 0;
+    
+            // Stop Music
+            audioMusic.Stop();
+    
+            // Destroy
+            //Destroy (this);
+        }
 }
