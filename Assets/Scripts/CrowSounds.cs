9c4f5cb4f3f669668ff9076c954d181f8e997f18
Kelson <shquapp@gmail.com>
2023-06-07 20:19:17 -0700

    Worker Controller

--- Assets/Scripts/CrowSounds.cs
+++ Assets/Scripts/CrowSounds.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CrowSounds : MonoBehaviour
 {
     public AudioSource crowAudio;
+    public AudioSource truckAudio;
     private float randomTime = 5;
     private float timeCounter = 0;
+    private int zone = 1;
 
     void Update()
     {
@@ -14,8 +16,14 @@ public class CrowSounds : MonoBehaviour
         {
             randomTime = Random.Range(10,20);
             timeCounter = 0;
-            crowAudio.Play();
-        }
+            if(zone == 1)
+            {
+                crowAudio.Play();
+            }
+            if(zone == 2)
+            {
+                truckAudio.Play();
+            }
         
         timeCounter += Time.deltaTime;
     }
